db590dc43f28f1b6cd454b836a497dd7f6f02d73
GitHub Action <action@github.com>
2023-02-09 10:37:14 +0000

    Template cleanup

--- src/main/kotlin/com/github/irtimir/intellijplatformpluginlinktoremote/services/MyProjectService.kt
+++ src/main/kotlin/com/github/irtimir/intellijplatformpluginlinktoremote/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.irtimir.intellijplatformpluginlinktoremote.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.irtimir.intellijplatformpluginlinktoremote.MyBundle
 
 class MyProjectService(project: Project) {
 
