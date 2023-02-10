635dceaf200cc388463ec9763ea641809cc5d2a7
ilias <irtimirbulatov@gmail.com>
2023-02-10 19:42:35 +0300

    delete tests

--- src/test/kotlin/com/github/irtimir/intellijplatformpluginlinktoremote/MyPluginTest.kt
+++ src/test/kotlin/com/github/irtimir/intellijplatformpluginlinktoremote/MyPluginTest.kt
@@ -1,12 +1,10 @@
 package com.github.irtimir.intellijplatformpluginlinktoremote
 
 import com.intellij.ide.highlighter.XmlFileType
-import com.intellij.openapi.components.service
 import com.intellij.psi.xml.XmlFile
 import com.intellij.testFramework.TestDataPath
 import com.intellij.testFramework.fixtures.BasePlatformTestCase
 import com.intellij.util.PsiErrorElementUtil
-import com.github.irtimir.intellijplatformpluginlinktoremote.services.MyProjectService
 
 @TestDataPath("\$CONTENT_ROOT/src/test/testData")
 class MyPluginTest : BasePlatformTestCase() {
@@ -30,9 +28,7 @@ class MyPluginTest : BasePlatformTestCase() {
     }
 
     fun testProjectService() {
-        val projectService = project.service<MyProjectService>()
 
-        assertEquals(4, projectService.getRandomNumber())
     }
 
     override fun getTestDataPath() = "src/test/testData/rename"
